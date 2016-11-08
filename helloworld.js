6cf95bce927e9c31a96f5e1c037bc2b6711c91f5
James Hines <jameshines10@gmail.com>
2016-11-08 09:04:31 -0500

    Trying to figure out why context.done does not log anything to cloudwatch

--- helloworld.js
+++ helloworld.js
@@ -1,9 +1,9 @@
 console.log('Loading event...');
 
-exports.handler = function(event, context) {
+exports.handler = function(event, context, callback) {
 	console.log('value1 = ' + event.key1);
 	console.log('value2 = ' + event.key2);
 	console.log('value3 = ' + event.key3);
 	
-	context.done(null, 'Hello World!');  // SUCCESS with message
+	callback(null, "Hello World!");  // SUCCESS with message
 }
